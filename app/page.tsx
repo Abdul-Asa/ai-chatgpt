1d30bc74da124ffe0bb3d339b67bd2c4fbcb79f0
Your Name <bigtulahi1@gmaiil.com>
2023-08-12 17:33:09 +0100

    try again

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,31 @@
-// `app/page.tsx` is the UI for the `/` URL
-export default function Page() {
-  return <main className="mx-auto w-full h-screen ">Hello, Home page!</main>;
-}
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <main className="min-h-screen mx-a bg-gray-100">
+      <div className="bg-blue-500 py-12">
+        <div className="container mx-auto px-4">
+          <h1 className="text-4xl font-bold text-white mb-4">
+            Welcome to my website!
+          </h1>
+          <p className="text-lg text-white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+      <div className="py-12">
+        <div className="container mx-auto px-4">
+          <div className="bg-white rounded-lg shadow-lg p-8">
+            <h2 className="text-2xl font-bold mb-4">Get in touch with us!</h2>
+            <p className="text-lg mb-4">
+              Have any questions or just want to say hello?
+            </p>
+            <Link href="/chat">Start chatting now</Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
